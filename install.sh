db3ea478f7ac21b5c32c837f4083334148f71101
mark-durbin-ucf <75274775+mark-durbin-ucf@users.noreply.github.com>
2021-03-30 12:17:24 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Content from https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+
+# Download NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# Activate NVM
+. ~/.nvm/nvm.sh
+
+# Install latest version of Node.js
+nvm install node
+
+# CHECK: Log installed version
+node -e "console.log('Running Node.js ' + process.version)"
